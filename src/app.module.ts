5db3baf6ddde9f5942739fe8c9f5502774fa701b
ksmarianique <ksmarianique@gmail.com>
2023-04-12 00:09:49 +0100

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { DesArticlesModule } from './des-articles/des-articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+//import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [PrismaModule, DesArticlesModule, UsersModule],
+  imports: [PrismaModule, DesArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
