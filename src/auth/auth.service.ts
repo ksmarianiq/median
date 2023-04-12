a9e427d4206fe48be21789be5d19fbcb7036b208
ksmarianique <ksmarianique@gmail.com>
2023-04-12 15:39:46 +0100

    yet commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,7 +7,7 @@ import {
 import { PrismaService } from './../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthEntity } from './entity/auth.entity';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
@@ -22,8 +22,8 @@ export class AuthService {
     }
 
     // Step 2: Check if the password is correct
-    const isPasswordValid = user.password === password;
-
+    //const isPasswordValid = user.password === password;
+    const isPasswordValid = await bcrypt.compare(password, user.password);
     // If password does not match, throw an error
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid password');
--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
-    .setVersion('0.1')
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
